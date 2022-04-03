59e07b74bb4bf3ac5771cbc7ef65d1a28930748d
krishnamurthypranesh <pk96ishere@gmail.com>
2022-04-03 23:31:20 +0530

    :sparkles: Add constants for days of the week

--- constants/common.py
+++ constants/common.py
@@ -6,7 +6,10 @@ from dataclasses import dataclass
 @dataclass
 class CommonConstantsClass:
     seconds_in_a_day: int
+    days_of_the_week: typing.List[str]
 
 CommonConstants = CommonConstantsClass(
             seconds_in_a_day=1*86400,
+            days_of_the_week=['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+                'Friday', 'Saturday', 'Sunday',],
         )
